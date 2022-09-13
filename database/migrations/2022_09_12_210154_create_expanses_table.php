2e11d694e5b345e4168a1e5e33caf6b47bd13e35
zucolli <carlos.zucolli@gmail.com>
2022-09-13 20:19:31 -0300

    Iniciando Projeto

---
+++ database/migrations/2022_09_12_210154_create_expanses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expanses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user')->constrained()->onDelete('cascade');
+            $table->string('description');
+            $table->integer('type');
+            $table->integer('amount'); //29,90 -> 2990
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expanses');
+    }
+};
